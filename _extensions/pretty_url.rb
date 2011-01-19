b6d0b863630006b739a328f12ce5f58320493b1b
Octave Zangs <octavez@octavez.com>
2011-01-19 14:28:55 +0100

    Added support for staging urls

--- _extensions/pretty_url.rb
+++ _extensions/pretty_url.rb
@@ -3,9 +3,13 @@ module Jekyll
   module Filters
     
     def pretty_url(url)
-      if ENV['RACK_ENV'] == 'production'
+      case ENV['RACK_ENV']
+      when 'production'
         url.gsub! /\/$/, ''
         'http://blog.jilion.com' + url
+      when 'staging'
+        url.gsub! /\/$/, ''
+        'http://blogjilion-staging.heroku.com' + url
       else
         'http://localhost:4000' + url
       end
