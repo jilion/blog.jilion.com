f84a8eccdbe2004126aa224213ca2a901c0781f5
Rémy Coutable <remy@jilion.com>
2013-05-23 18:33:20 +0200

    Fix compile task

--- _extensions/pretty_url.rb
+++ _extensions/pretty_url.rb
@@ -3,7 +3,6 @@ module Jekyll
   module Filters
 
     def pretty_url(url)
-      puts "ENV['RACK_ENV'] : #{ENV['RACK_ENV']}"
       case ENV['RACK_ENV']
       when 'production'
         url.gsub! /\/$/, ''
